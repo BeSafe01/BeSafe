1d80466c06aec4f0d568f589b36422fe5019688f
Matheus-Venancio <matheusvecordeiro@gmail.com>
2022-01-18 19:18:33 -0300

    Initial RegisterActivityAttendant.kt
    Finish field limited

---
+++ app/src/main/java/com/example/safe/RegisterActivityAttendant.kt
@@ -0,0 +1,11 @@
+package com.example.safe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivityAttendant : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_attendant)
+    }
+}
