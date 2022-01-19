444af83277fcda7610fb014dcbf3749a5fc6fbeb
Matheus-Venancio <matheusvecordeiro@gmail.com>
2022-01-18 22:40:41 -0300

    developing RegisterActivityAttendant.kt
    need to test RegisterActivityAttendant.kt

--- app/src/main/java/com/example/safe/RegisterActivityAttendant.kt
+++ app/src/main/java/com/example/safe/RegisterActivityAttendant.kt
@@ -1,11 +1,54 @@
 package com.example.safe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.safe.databinding.ActivityInitialBinding
+import com.example.safe.databinding.ActivityRegisterAttendantBinding
+import com.example.safe.databinding.ActivityRegisterUserBinding
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivityAttendant : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterAttendantBinding
+    val db = Firebase.firestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_attendant)
+        binding = ActivityRegisterAttendantBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnRegisterAttendent.setOnClickListener {
+            //Mudar && para ||, preencha os
+            if (binding.edtFullNameAttendant.text.isEmpty() || binding.edtEmailAttendant.text.isEmpty() ||
+                binding.edtPasswordAttendant.text.isEmpty() || binding.edtPasswordAttendant.text.isEmpty() ||
+                binding.edtCpfAttendant.text.isEmpty() || binding.edtPhoneAttendant.text.isEmpty() ||
+                binding.edtIdentificationCodeAttendant.text.isEmpty() || binding.edtDateBirthAttendant.text.isEmpty()){
+
+                Toast.makeText(this,"Preencha os campos vazio", Toast.LENGTH_SHORT).show()
+
+            }else{
+                val user = UserModel(binding.edtFullNameAttendant.text.toString(), binding.edtEmailAttendant.text.toString(),
+                    binding.edtPasswordAttendant.text.toString(), binding.edtCpfAttendant.text.toString() ,
+                    binding.edtPhoneAttendant.text.toString()  ,binding.edtIdentificationCodeAttendant.text.toString(),binding.edtDateBirthAttendant.text.toString() )
+
+                db.collection("users")//Name Collection
+                    .document(binding.edtCpfAttendant.text.toString())//Reference id: Cpf
+                    .set(user)//put data in the database
+
+                    .addOnSuccessListener {
+                        Toast.makeText(this,"Cadastrado com sucesso", Toast.LENGTH_SHORT).show()
+                        val intent = Intent (this, LoginActivity::class.java)
+                        startActivity(intent)
+                    }
+
+                    .addOnFailureListener(OnFailureListener {
+                        Toast.makeText(this,"Revise seus dados, Usuario não cadastrado", Toast.LENGTH_SHORT).show()
+                    })
+            }
+
+        }
     }
 }
